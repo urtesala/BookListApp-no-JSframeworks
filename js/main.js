d4af5a4952b9fc1a8c755ef696d8d26729bf36b3
urtesala <urtesaladziute@gmail.com>
2022-11-05 19:11:49 +0300

    JS Book and UI Classes done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,58 @@
-'use strict';
-console.log('main.js');
+"use strict";
+console.log("main.js");
+
+//^ Book Class: Represents a book
+
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+//^ UI Class: Handle UI Tasks
+
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: "Sapiens: A Brief History of Humankind",
+        author: " Yuval Noah Harari",
+        isbn: "567733823",
+      },
+      {
+        title: "Crowds and Power",
+        author: "Elias Canetti",
+        isbn: "512121543",
+      },
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.getElementById("book-list");
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="btn btn-outline-danger btn-sm delete"></a>X</td>
+    `;
+
+    list.appendChild(row);
+  }
+}
+
+//^ Store Class: Handle Storage
+
+//^ Event: Display Books
+
+//^ Event: Add a Book
+
+//^ Event: Remove a Book
